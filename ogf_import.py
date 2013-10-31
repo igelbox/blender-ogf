8b687ddf006ba366b3f26be55f61c925db70118f
Vakhurin Sergey <igelbox@gmail.com>
2013-10-31 23:38:46 +0400

    first working version

--- ogf_import.py
+++ ogf_import.py
@@ -6,72 +6,105 @@ class Chunks:
     OGF_HEADER      = 0x1
     OGF4_S_DESC     = 0x12
     OGF4_CHILDREN   = 0x9
+    OGF4_TEXTURE    = 0x2
+    OGF4_VERTICES   = 0x3
+    OGF4_INDICES    = 0x4
 
-class Chunk:
-    def __init__(self, id, data):
-        self.id = id
-        self.data = data
+class rawr:
+    def __init__(self, data):
         self.offs = 0
-    def read(self, fmt):
-        v = struct.unpack_from(fmt, self.data, self.offs)
-        self.offs += struct.calcsize(fmt);
+        self.data = data
+    def read(sz=1):
+        v = data[self.offs:self.offs+sz]
+        self.offs+=sz
         return v;
-    def read_asciiz(self):
+    def unpack(self, fmt):
+        s = struct.calcsize(fmt)
+        self.offs += s
+        return struct.unpack_from(fmt, self.data, self.offs-s)
+    def unpack_asciiz(self):
         zpos = self.data.find(0, self.offs);
         if (zpos == -1):
             zpos = len(self.data);
-        return self.read('={}sx'.format(zpos - self.offs))[0];
-    def read_reader(self):
-        def rr(sz=1):
-            v = self.data[self.offs:self.offs+sz]
-            self.offs += sz
-            return v
-        return ChunkReader(rr)
-class ChunkReader:
+        return self.unpack('={}sx'.format(zpos - self.offs))[0].decode('cp1251');
+def ogfr(data):
     MASK_COMPRESSED = 0x80000000
-    MASK_ID         = ~MASK_COMPRESSED
-    def __init__(self, read):
-        self.read = read
-    def next(self, check=0):
-        b = self.read(8)
-        if (len(b) == 0):
-            return None
-        i, s = struct.unpack('=II', b);
-        if ((i & self.MASK_COMPRESSED) != 0):
+    offs = 0
+    while (offs < len(data)):
+        i, s = struct.unpack_from('=II', data, offs);
+        if ((i & MASK_COMPRESSED) != 0):
             raise Exception('compressed')
-        i &= self.MASK_ID
-        if ((check != 0) & (i != check)):
-            raise Exception('expected chunk {}, but found: {}'.format(check, i))
-        return Chunk(i, self.read(s))
+        offs += 8 + s
+        yield (i & ~MASK_COMPRESSED, data[offs-s:offs])
+def cfrs(tupl, expected):
+    if (tupl[0] != expected):
+        raise Exception('expected {}, but found: {}'.format(expected, tupl[0]))
+    return tupl[1]
 
-def load_ogfX(h, r):
+def load_ogfX(h, ogr):
     raise Exception('unsupported OGF format version: {}'.format(h[0]))
 
-def load_ogf4(h, r):
-    mt, shid = h.read('=BH')
-    #~ print ('modeltype:{}, shaderid:{}'.format(mt, shid))
-    bbox = h.read('=ffffff')
-    #~ print ('bbox:{}'.format(bbox))
-    bsphere = h.read('=ffff')
-    #~ print ('bsphere:{}'.format(bsphere))
-    c = r.next(Chunks.OGF4_S_DESC)
-    src = c.read_asciiz()
+def load_ogf4_m05(ogr):
+    c = rawr(cfrs(next(ogr), Chunks.OGF4_TEXTURE))
+    tex = c.unpack_asciiz()
+    shd = c.unpack_asciiz()
+    #~ print ('texture:{}, shader:{}'.format(tex, shd));
+    c = rawr(cfrs(next(ogr), Chunks.OGF4_VERTICES))
+    vf, vc = c.unpack('=II')
+    if (vf != 0x12071980):
+        raise Exception('expected vertex format {:#x}, but found: {:#x}'.format(0x12071980, vf))
+    vv = []
+    #~ print('vf:{:#x}, vc:{}'.format(vf, vc))
+    for _ in range(vc):
+        v = c.unpack('=fff')
+        vv.append(v)
+        n = c.unpack('=fff')
+        c.unpack('=fff')#tangen
+        c.unpack('=fff')#binorm
+        t = c.unpack('=ff')
+        f = c.unpack('=I')[0]
+        #~ print(v, n, b, t, f)
+    c = rawr(cfrs(next(ogr), Chunks.OGF4_INDICES))
+    ic = c.unpack('=I')[0]
+    ii = []
+    for _ in range(ic//3):
+        ii.append(c.unpack('=HHH'))
+    #~ print('{},[],{}'.format(vv, ii))
+    return (vv, ii)
+def load_ogf4_m10(ogr):
+    c = rawr(cfrs(next(ogr), Chunks.OGF4_S_DESC))
+    src = c.unpack_asciiz()
     #~ print ('source:{}'.format(src));
-    exptool = c.read_asciiz()
-    exptime, crttime, modtime = c.read('=III')
-    c = r.next(Chunks.OGF4_CHILDREN)
-    cr = c.read_reader()
+    exptool = c.unpack_asciiz()
+    exptime, crttime, modtime = c.unpack('=III')
+    result = []
+    for i, c in ogfr(cfrs(next(ogr), Chunks.OGF4_CHILDREN)):
+        result.append(load_ogf(c))
+    return result
+
+def load_ogf4(h, ogr):
+    mt, shid = h.unpack('=BH')
+    print ('modeltype:{}, shaderid:{}'.format(mt, shid))
+    bbox = h.unpack('=ffffff')
+    #~ print ('bbox:{}'.format(bbox))
+    bsphere = h.unpack('=ffff')
+    #~ print ('bsphere:{}'.format(bsphere))
+    return {
+     5: load_ogf4_m05,
+    10: load_ogf4_m10
+    }.get(mt)(ogr)
+
+def load_ogf(data):
+    ogr = ogfr(data)
+    cr = rawr(cfrs(next(ogr), Chunks.OGF_HEADER))
+    ver = cr.unpack('=B')[0]
+    #~ print ('version:{}'.format(ver))
+    return {
+     4: load_ogf4
+    }.get(ver, load_ogfX)(cr, ogr)
 
 def load(fname):
-    f = io.open(fname, 'rb')
-    try:
-        r = ChunkReader(f.read)
-        c = r.next(Chunks.OGF_HEADER)
-        ver = c.read('=B')[0]
-        print ('header(size:{}, version:{})'.format(len(c.data), ver))
-        {4: load_ogf4
-        }.get(ver, load_ogfX)(c, r)
-    finally:
-        f.close();
-
-load('test.ogf')
+    with io.open(fname, mode = 'rb') as f:
+        return load_ogf(f.read())
+if __name__ == '__main__':
+    load('test.ogf')
