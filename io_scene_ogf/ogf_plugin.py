4e29210ec2026f69b6d4320fe44bf853955c9b37
Vakhurin Sergey <igelbox@gmail.com>
2013-11-12 00:47:30 +0400

    oops! missing bl_info

--- io_scene_ogf/ogf_plugin.py
+++ io_scene_ogf/ogf_plugin.py
@@ -1,16 +1,3 @@
-bl_info = {
-    'name':     'OGF Tools',
-    'author':   'Vakhurin Sergey (igel)',
-    'version':  (0, 0, 0),
-    'blender':  (2, 68, 0),
-    'category': 'Import-Export',
-    'location': 'File > Import/Export',
-    'description': 'Import STALKER OGF files',
-    'wiki_url': 'https://github.com/igelbox/blender-ogf',
-    'tracker_url': 'https://github.com/igelbox/blender-ogf/issues',
-    'warning':  'Under construction!'
-}
-
 import bpy
 
 
